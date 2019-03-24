9353b8088f2e033e1355c45b5bb2542471d2a0cd
peshala <peshaladayanatha@gmail.com>
2019-03-24 11:43:04 +0530

    backup

--- publicera/src/app/app-routing.module.ts
+++ publicera/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page/landing-page.component';
+import { LoginComponent } from './public/login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- publicera/src/app/app.module.ts
+++ publicera/src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//modules 
+import { CoreModule } from './core/core.module';
+import { LandingPageModule } from './landing-page/landing-page.module';
+import { SharedModule } from './shared/shared.module';
+import { PublicModule } from './public/public.module';
+import { SecureModule } from './secure/secure.module';
+
+
+////
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    LandingPageModule,
+    SharedModule,
+    PublicModule,
+    SecureModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
