6dc64235caae9b67b4467fb1107f055273d9e7a5
Avindu Hewa <avinduhewa@gmail.com>
2019-03-25 19:56:58 +0530

    login navigation

--- publicera/src/app/app-routing.module.ts
+++ publicera/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './public/landing-page/landing-page.component';
-import { LoginComponent } from './public/login/login.component'
 
 const routes: Routes = [
-
   { path: 'home', component: LandingPageComponent },
 ];
 
