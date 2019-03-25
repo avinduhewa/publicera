fad687fafd32a0231323e12a15c60c15e7e8d434
Avindu Hewa <avinduhewa@gmail.com>
2019-03-25 22:21:50 +0530

    implement RSS feed for blog articles

--- publicera/src/app/shared/components/artcle-card-horizontal/artcle-card-horizontal.component.ts
+++ publicera/src/app/shared/components/artcle-card-horizontal/artcle-card-horizontal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-artcle-card-horizontal',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artcle-card-horizontal.component.scss']
 })
 export class ArtcleCardHorizontalComponent implements OnInit {
-
+  @Input() feed;
   constructor() { }
 
   ngOnInit() {
