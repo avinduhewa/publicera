a32c2065fad5e8ed6ef72b1b129913f62ce5c921
peshala <peshaladayanatha@gmail.com>
2019-05-22 21:30:02 +0530

    new landing page doc

---
+++ 2.0/ngx-admin/src/app/public/reelancer-register/reelancer-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reelancer-register',
+  templateUrl: './reelancer-register.component.html',
+  styleUrls: ['./reelancer-register.component.scss']
+})
+export class ReelancerRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
